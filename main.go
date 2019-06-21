ed4dbda6e4c8b091b41bc7f9eed62f3d75f160a9
Peng <peng.devs@gmail.com>
2019-06-21 14:57:32 +0800

    Use log module to log message and running server in goroutine

--- main.go
+++ main.go
@@ -1,28 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
-	"os"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("[/hello] has bean called")
+	log.Println("[/hello] has bean called")
 	w.Write([]byte("Hi, I'm Go server."))
 }
 
 func health(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("[/health] has been called")
+	log.Println("[/health] has been called")
 	w.WriteHeader(http.StatusOK)
 }
 
 func main() {
-	var port = ":" + os.Getenv("APP_PORT")
-	fmt.Println("start server at " + port)
-
 	http.HandleFunc("/hello", hello)
 	http.HandleFunc("/health", health)
-	if err := http.ListenAndServe(port, nil); err != nil {
-		panic(err)
-	}
+
+	done := make(chan bool)
+	go http.ListenAndServe(":8080", nil)
+	log.Println("Server started")
+	<-done
 }
