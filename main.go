9835c6be89d4ac4d042745a83cb61885b1d70fdb
Peng <peng.devs@gmail.com>
2019-06-21 17:29:38 +0800

    Add greet API for calling ktor hello API

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
@@ -10,14 +12,37 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hi, I'm Go server."))
 }
 
+func greeting(host string) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println("[/greet/ktor] has been called")
+
+		resp, err := http.Get("http://" + host + "/hello")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.Write(body)
+	}
+}
+
 func health(w http.ResponseWriter, r *http.Request) {
-	log.Println("[/health] has been called")
 	w.WriteHeader(http.StatusOK)
 }
 
 func main() {
-	http.HandleFunc("/hello", hello)
+	host := os.Getenv("KTOR_HOST")
+
 	http.HandleFunc("/health", health)
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/greet/ktor", greeting(host))
 
 	done := make(chan bool)
 	go http.ListenAndServe(":8080", nil)
