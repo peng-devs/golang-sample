7411fd5462e307e795ff70e02501040f76082616
Peng <peng.devs@gmail.com>
2019-06-21 14:13:49 +0800

    Add health check API

--- main.go
+++ main.go
@@ -10,8 +10,14 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hi, I'm Go server."))
 }
 
+func health(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("[/health] recieve request")
+	w.WriteHeader(http.StatusOK)
+}
+
 func main() {
 	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/health", health)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
